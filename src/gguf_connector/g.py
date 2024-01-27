4be4e01fa3b0539e926bbb34c68f3a12f654edec
calcuis <113646141+calcuis@users.noreply.github.com>
2024-01-28 07:38:41 +0800

    Create g.py

---
+++ src/gguf_connector/g.py
@@ -0,0 +1,37 @@
+import os
+
+gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
+
+if gguf_files:
+    print("GGUF file(s) available. Select which one to use:")
+    
+    for index, file_name in enumerate(gguf_files, start=1):
+        print(f"{index}. {file_name}")
+
+    choice = input(f"Enter your choice (1 to {len(gguf_files)}): ")
+    
+    try:
+        choice_index=int(choice)-1
+        selected_file=gguf_files[choice_index]
+        print(f"Model file: {selected_file} is selected!")
+        ModelPath=selected_file
+
+        from ctransformers import AutoModelForCausalLM
+        llm = AutoModelForCausalLM.from_pretrained(ModelPath)
+
+        while True:
+            ask = input("Enter a Question (Q for quit): ")
+
+            if ask == "q" or ask == "Q":
+                  break
+
+            ans = llm(ask)
+            print(ask+ans)
+
+    except (ValueError, IndexError):
+        print("Invalid choice. Please enter a valid number.")
+else:
+    print("No GGUF files are available in the current directory.")
+    input("--- Press ENTER To Exit ---")
+
+print("Goodbye!")
