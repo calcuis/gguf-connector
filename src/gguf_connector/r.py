cd5139cb1be48ec262d54d74e5e87479d1fbdbca
calcuis <113646141+calcuis@users.noreply.github.com>
2024-06-27 08:48:59 +0800

    Update r.py

--- src/gguf_connector/r.py
+++ src/gguf_connector/r.py
@@ -38,5 +38,4 @@ if gguf_files:
         print("Invalid choice. Please enter a valid number.")
 else:
     print("No GGUF files are available in the current directory.")
-    input("--- Press ENTER To Exit ---")
-# print("Goodbye!")
+    input("--- Press ENTER To Skip ---")
