6b49632a5b129df44b4d108cfd18a697d67c93e7
calcuis <113646141+calcuis@users.noreply.github.com>
2024-10-09 08:08:57 +0800

    Update r.py

--- src/gguf_connector/r.py
+++ src/gguf_connector/r.py
@@ -2,18 +2,17 @@
 def read_gguf_file(gguf_file_path):
     from gguf_connector.reader import GGUFReader
     reader = GGUFReader(gguf_file_path)
-
+    print("-" * 105)
     print("Key-Value Pairs:")
     max_key_length = max(len(key) for key in reader.fields.keys())
     for key, field in reader.fields.items():
         value = field.parts[field.data[0]]
         print(f"{key:{max_key_length}} : {value}")
-    print("----")
-
+    print("-" * 105)
     print("Tensors:")
     tensor_info_format = "{:<30} | Shape: {:<15} | Size: {:<12} | Quantization: {}"
     print(tensor_info_format.format("Tensor Name", "Shape", "Size", "Quantization"))
-    print("-" * 80)
+    print("-" * 105)
     for tensor in reader.tensors:
         shape_str = "x".join(map(str, tensor.shape))
         size_str = str(tensor.n_elements)
@@ -24,7 +23,7 @@ import os
 gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
 
 if gguf_files:
-    print("GGUF file(s) available. Select which one to read:")
+    print("GGUF file(s) available. Select which one to read:")   
     for index, file_name in enumerate(gguf_files, start=1):
         print(f"{index}. {file_name}")
     choice = input(f"Enter your choice (1 to {len(gguf_files)}): ")
@@ -33,9 +32,14 @@ if gguf_files:
         selected_file=gguf_files[choice_index]
         print(f"Model file: {selected_file} is selected!")
         ModelPath=selected_file
-        read_gguf_file(ModelPath)
+
+        from rich.progress import Progress
+        with Progress(transient=True) as progress:
+            task = progress.add_task("Processing", total=None)
+            read_gguf_file(ModelPath)
+
     except (ValueError, IndexError):
         print("Invalid choice. Please enter a valid number.")
 else:
     print("No GGUF files are available in the current directory.")
-    input("--- Press ENTER To Skip ---")
+    input("--- Press ENTER To Exit ---")
