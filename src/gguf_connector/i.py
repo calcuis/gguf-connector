16f0d6e3d5139b7e46759534f24c9e125cbc5abe
calcuis <113646141+calcuis@users.noreply.github.com>
2024-10-28 08:48:23 +0800

    Update i.py

--- src/gguf_connector/i.py
+++ src/gguf_connector/i.py
@@ -1,3 +1,4 @@
 import webbrowser
 print("activating the default browser...")
 webbrowser.open("https://gguf.io")
+print("note: if you are encountering restricted access problem with this domain; visit https://gguf.us instead; same content")
