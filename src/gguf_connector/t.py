617f71c51acbb8d006a1d3cf2111dc43f6cf3a92
calcuis <113646141+calcuis@users.noreply.github.com>
2024-12-27 15:17:47 +0800

    Update t.py

--- src/gguf_connector/t.py
+++ src/gguf_connector/t.py
@@ -22,14 +22,15 @@ class ModelHYVID(ModelTemplate):
         ("transformer_blocks.0.attn.norm_added_k.weight",),
         ("double_blocks.0.img_attn.proj.weight",),
     ]
-
-class ModelFlux(ModelTemplate):
-    arch = "flux"
-    keys_detect = [
-        ("transformer_blocks.0.attn.norm_added_k.weight",),
-        ("double_blocks.0.img_attn.proj.weight",),
-    ]
     keys_banned = ["transformer_blocks.0.attn.norm_added_k.weight",]
+
+# class ModelFlux(ModelTemplate):
+#     arch = "flux"
+#     keys_detect = [
+#         ("transformer_blocks.0.attn.norm_added_k.weight",),
+#         ("double_blocks.0.img_attn.proj.weight",),
+#     ]
+#     keys_banned = ["transformer_blocks.0.attn.norm_added_k.weight",]
 
 class ModelSD3(ModelTemplate):
     arch = "sd3"
@@ -80,7 +81,8 @@ class ModelSD1(ModelTemplate):
         ), # Non-diffusers
     ]
 
-arch_list = [ModelFlux, ModelSD3, ModelAura, ModelLTXV, ModelHYVID, ModelSDXL, ModelSD1]
+# arch_list = [ModelFlux, ModelSD3, ModelAura, ModelLTXV, ModelHYVID, ModelSDXL, ModelSD1]
+arch_list = [ModelSD3, ModelAura, ModelLTXV, ModelHYVID, ModelSDXL, ModelSD1]
 
 def is_model_arch(model, state_dict):
     matched = False
