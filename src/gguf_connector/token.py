77a7c6cb7159d631131fbafb9c9c28fc4931e904
calcuis <113646141+calcuis@users.noreply.github.com>
2025-03-16 11:38:27 -0800

    Update token.py

--- src/gguf_connector/token.py
+++ src/gguf_connector/token.py
@@ -26,20 +26,19 @@ def get_list_field(reader, field_name, field_type):
     else:
         raise TypeError(f"Unknown field type {field_type}")
 
-def gguf_tokenizer_loader(path, temb_shape):
-    print(f"Attempting to rebuild sentencepiece tokenizer from metadata..")
+def gguf_tokenizer_loader(path):
+    print(f'Attempting to rebuild sentencepiece tokenizer from metadata..')
     try:
         from sentencepiece import sentencepiece_model_pb2 as model
     except ImportError:
-        raise ImportError("protobuf is required; pip install protobuf")
+        raise ImportError('protobuf is required; pip install protobuf')
     spm = model.ModelProto()
     reader = GGUFReader(path)
     spm.trainer_spec.model_type == 1
     spm.normalizer_spec.add_dummy_prefix = get_field(reader, "tokenizer.ggml.add_space_prefix", bool)
-    spm.normalizer_spec.remove_extra_whitespaces = get_field(reader, "tokenizer.ggml.remove_extra_whitespaces", bool)
-    tokens = get_list_field(reader, "tokenizer.ggml.tokens", str)
-    scores = get_list_field(reader, "tokenizer.ggml.scores", float)
-    toktypes = get_list_field(reader, "tokenizer.ggml.token_type", int)
+    tokens = get_list_field(reader, 'tokenizer.ggml.tokens', str)
+    scores = get_list_field(reader, 'tokenizer.ggml.scores', float)
+    toktypes = get_list_field(reader, 'tokenizer.ggml.token_type', int)
     for idx, (token, score, toktype) in enumerate(zip(tokens, scores, toktypes)):
         piece = spm.SentencePiece()
         piece.piece = token
@@ -49,8 +48,9 @@ def gguf_tokenizer_loader(path, temb_shape):
     spm.trainer_spec.byte_fallback = True
     spm.trainer_spec.vocab_size = len(tokens)
     spm.trainer_spec.max_sentence_length = 4096
-    spm.trainer_spec.eos_id = get_field(reader, "tokenizer.ggml.eos_token_id", int)
-    spm.trainer_spec.pad_id = get_field(reader, "tokenizer.ggml.padding_token_id", int)
-    print(f"Rebuilt tokenizer successfully with vocab size of {len(spm.pieces)}")
+    spm.trainer_spec.eos_id = get_field(reader,
+        'tokenizer.ggml.eos_token_id', int)
+    spm.trainer_spec.pad_id = get_field(reader, 'tokenizer.ggml.padding_token_id', int)
+    print(f'Rebuilt tokenizer successfully with vocab size of {len(spm.pieces)}')
     del reader
     return torch.ByteTensor(list(spm.SerializeToString()))
