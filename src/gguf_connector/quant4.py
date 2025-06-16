13181bb5ddd009cb454d7b558f8d0b7d5a384b06
calcuis <113646141+calcuis@users.noreply.github.com>
2025-06-16 01:19:52 -0800

    Update quant4.py

--- src/gguf_connector/quant4.py
+++ src/gguf_connector/quant4.py
@@ -1,10 +1,9 @@
 
 import torch # need torch to work; pip install torch
 from safetensors.torch import load_file
-import os
-def convert_safetensors_to_pth(safetensors_path):
+
+def convert_safetensors_to_pth(safetensors_path, output_path):
     print(f"Loading .safetensors file: {safetensors_path}")
     state_dict = load_file(safetensors_path)
-    output_path = os.path.splitext(safetensors_path)[0] + ".pth"
     print(f"Packing to .pth file: {output_path}")
-    torch.save(state_dict, output_path
+    torch.save(state_dict, output_path)
