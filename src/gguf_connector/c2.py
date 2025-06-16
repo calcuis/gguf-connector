893c0d5e19350ce6087f1802eecd67d9d476e09f
calcuis <113646141+calcuis@users.noreply.github.com>
2025-06-16 07:01:23 -0800

    Update c2.py

--- src/gguf_connector/c2.py
+++ src/gguf_connector/c2.py
@@ -131,7 +131,7 @@ if gguf_files:
     except (ValueError, IndexError):
         print("Invalid choice. Please enter a valid number.")
 else:
-    print("No GGUF/Safetensors are available in the current directory.")
+    print("No GGUF files are available in the current directory.")
     input("--- Press ENTER To Exit ---")
 
 @dataclass
