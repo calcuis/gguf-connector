dedffbc57b8c662b4a28b48e7851dca6a84d6973
calcuis <113646141+calcuis@users.noreply.github.com>
2025-07-10 12:03:01 -0800

    Create quant2b.py

---
+++ src/gguf_connector/quant2b.py
@@ -0,0 +1,21 @@
+
+import torch # need torch to work
+
+# add split block by dims
+def split_block_dims(blocks, *args):
+    n_max = blocks.shape[1]
+    dims = list(args) + [n_max - sum(args)]
+    return torch.split(blocks, dims, dim=1)
+# unit conversion (for 5_0, 5_1)
+def to_uint32(x):
+    x = x.view(torch.uint8).to(torch.int32)
+    return (x[:, 0] | x[:, 1] << 8 | x[:, 2] << 16 | x[:, 3] << 24).unsqueeze(1)
+# calculate scale min (for 4_k, 5_k)
+def get_scale_min(scales):
+    n_blocks = scales.shape[0]
+    scales = scales.view(torch.uint8)
+    scales = scales.reshape((n_blocks, 3, 4))
+    d, m, m_d = torch.split(scales, scales.shape[-2] // 3, dim=-2)
+    sc = torch.cat([d & 0x3F, (m_d & 0x0F) | ((d >> 2) & 0x30)], dim=-1)
+    min = torch.cat([m & 0x3F, (m_d >> 4) | ((m >> 2) & 0x30)], dim=-1)
+    return (sc.reshape((n_blocks, 8)), min.reshape((n_blocks, 8)))
