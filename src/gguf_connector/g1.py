4156ed97c8366a14fb6841c59e3a5e4ac0f79649
calcuis <113646141+calcuis@users.noreply.github.com>
2025-07-21 08:30:50 -0800

    Update g1.py

--- src/gguf_connector/g1.py
+++ src/gguf_connector/g1.py
@@ -14,19 +14,19 @@ def generate_code(prompt):
     inputs = tokenizer(prompt, return_tensors="pt").to(DEVICE)
     outputs = model.generate(
         **inputs,
-        max_new_tokens=1400,
+        max_new_tokens=1600,
         pad_token_id=tokenizer.eos_token_id
     )
     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return generated_text
 
 # Gradio UI
-interface = gr.Interface(
+iface = gr.Interface(
     fn=generate_code,
     inputs=gr.Textbox(lines=4, label="Enter Prompt", value="write a Solidity function to transfer tokens"),
-    outputs=gr.Textbox(lines=20, label="Generated Solidity Code"),
+    outputs=gr.Code(lines=20, label="Generated Solidity Code", language="javascript"),
     title="Smart Contract Generator",
-    description="Enter a prompt to generate smart contract"
+    description="Enter a prompt to generate smart contract (solidity code)"
 )
 # Launch the app
-interface.launch()
+iface.launch()
