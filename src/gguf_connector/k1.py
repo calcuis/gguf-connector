1b47a35cd24896f84d7dcdb000400efb7600faa5
calcuis <113646141+calcuis@users.noreply.github.com>
2025-07-25 14:38:26 -0800

    Update k1.py

--- src/gguf_connector/k1.py
+++ src/gguf_connector/k1.py
@@ -5,6 +5,9 @@ from transformers import T5EncoderModel
 from diffusers import FluxKontextPipeline
 from PIL import Image
 
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"Using device: {DEVICE}")
+
 text_encoder = T5EncoderModel.from_pretrained(
     "calcuis/kontext-gguf",
     gguf_file="t5xxl_fp16-q4_0.gguf",
@@ -15,7 +18,7 @@ pipe = FluxKontextPipeline.from_pretrained(
     "calcuis/kontext-gguf",
     text_encoder_2=text_encoder,
     torch_dtype=torch.bfloat16
-    ).to("cuda")
+    ).to(DEVICE)
 
 ask=input("Choose plushie instead of bot lora (Y/n)? ")
 if ask.lower() == 'y':
