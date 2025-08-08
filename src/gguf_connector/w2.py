b6479fdd0055a8f8bf16cacffdd2326451878698
calcuis <113646141+calcuis@users.noreply.github.com>
2025-08-07 17:11:05 -0800

    Create w2.py

---
+++ src/gguf_connector/w2.py
@@ -0,0 +1,40 @@
+
+import torch # need torch to work; pip install torch
+from .w1 import launch_wan_app, launch_vace_app
+
+# connector selection (wan by default)
+ask=input("Use vace instead of wan (Y/n)? ")
+if ask.lower() == 'y':
+    selected = "vace"
+    print(f'connector choice: {selected}\n')
+else:
+    selected = "wan"
+    print(f'connector choice: {selected}\n')
+
+import os
+gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
+
+if gguf_files:
+    print('GGUF file(s) available. Select which one to use:')
+    for index, file_name in enumerate(gguf_files, start=1):
+        print(f'{index}. {file_name}')
+    choice = input(f'Enter your choice (1 to {len(gguf_files)}): ')
+    try:
+        choice_index = int(choice) - 1
+        selected_file = gguf_files[choice_index]
+        print(f'Model file: {selected_file} is selected as {selected} video generator!')
+        input_path = selected_file
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+        dtype = torch.bfloat16 if device == "cuda" else torch.float32
+        print(f"Device detected: {device}")
+        print(f"torch version: {torch.__version__}")
+        print(f"dtype using: {dtype}")
+        if selected == 'vace':
+            launch_vace_app(input_path, dtype)
+        else:
+            launch_wan_app(input_path, dtype)
+    except (ValueError, IndexError):
+        print('Invalid choice. Please enter a valid number.')
+else:
+    print('No GGUF files are available in the current directory.')
+    input('--- Press ENTER To Exit ---')
