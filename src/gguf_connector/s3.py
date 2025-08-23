e3bda323f365bd4f254e30fca345dee453e3b2f6
calcuis <113646141+calcuis@users.noreply.github.com>
2025-08-23 14:25:58 -0800

    Update s3.py

--- src/gguf_connector/s3.py
+++ src/gguf_connector/s3.py
@@ -29,6 +29,8 @@ if gguf_files:
         print(f"Device detected: {device}")
         print(f"torch version: {torch.__version__}")
         print(f"dtype using: {dtype}")
+        if device == "cuda":
+            print(f"running with: {torch.cuda.get_device_name(torch.cuda.current_device())}")
         if selected == 'large':
             launch_sd3_app(input_path, dtype)
         else:
