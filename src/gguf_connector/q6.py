0bb361547d9da0838c3120e8343a00d35b0e7be9
calcuis <113646141+calcuis@users.noreply.github.com>
2025-08-24 01:51:02 -0800

    Update q6.py

--- src/gguf_connector/q6.py
+++ src/gguf_connector/q6.py
@@ -1,15 +1,6 @@
 
 import torch # need torch to work; pip install torch
-from .q4 import launch_image_edit_app, launch_image_edit_low_app
-
-# vram mode selection (normal by default)
-ask=input("Use low vram mode instead (Y/n)? ")
-if ask.lower() == 'y':
-    selected = "low"
-    print(f'vram mode: {selected}\n')
-else:
-    selected = "normal"
-    print(f'vram mode: {selected}\n')
+from .q4 import launch_image_edit_app
 
 import os
 gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
@@ -22,7 +13,7 @@ if gguf_files:
     try:
         choice_index = int(choice) - 1
         selected_file = gguf_files[choice_index]
-        print(f'Model file: {selected_file} is selected as image editor - {selected} vram mode!')
+        print(f'Model file: {selected_file} is selected as image editor!')
         input_path = selected_file
         device = "cuda" if torch.cuda.is_available() else "cpu"
         dtype = torch.bfloat16 if device == "cuda" else torch.float32
@@ -31,10 +22,7 @@ if gguf_files:
         print(f"dtype using: {dtype}")
         if device == "cuda":
             print(f"running with: {torch.cuda.get_device_name(torch.cuda.current_device())}")
-        if selected == 'normal':
-            launch_image_edit_app(input_path,dtype)
-        else:
-            launch_image_edit_low_app(input_path,dtype)
+        launch_image_edit_app(input_path,dtype)
     except (ValueError, IndexError):
         print('Invalid choice. Please enter a valid number.')
 else:
