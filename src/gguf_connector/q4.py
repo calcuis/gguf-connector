7f6d7b7210e9e1830bac5c7cc699b59b7dc35781
calcuis <113646141+calcuis@users.noreply.github.com>
2025-08-24 01:53:32 -0800

    Update q4.py

--- src/gguf_connector/q4.py
+++ src/gguf_connector/q4.py
@@ -75,7 +75,7 @@ def launch_qi_distill_app(model_path,dtype):
         torch_dtype=dtype,
         config="callgg/qi-decoder",
         subfolder="transformer"
-        )
+    )
     text_encoder = Qwen2_5_VLForConditionalGeneration.from_pretrained(
         "callgg/qi-decoder",
         subfolder="text_encoder",
@@ -145,7 +145,7 @@ def launch_image_edit_app(model_path,dtype):
         "callgg/qi-decoder",
         subfolder="text_encoder",
         torch_dtype=dtype
-    )
+        )
     vae = AutoencoderKLQwenImage.from_pretrained(
         "callgg/qi-decoder",
         subfolder="vae",
@@ -173,67 +173,6 @@ def launch_image_edit_app(model_path,dtype):
     block = gr.Blocks(title="gguf").queue()
     with block:
         gr.Markdown("## 🐷 Image Editor")
-        with gr.Row():
-            with gr.Column():
-                input_image = gr.Image(type="pil", label="Input Image")
-                prompt = gr.Textbox(label="Prompt", placeholder="Enter your prompt here (or click Sample Prompt)", value="")
-                neg_prompt = gr.Textbox(label="Negative Prompt", value=" ", visible=False) # disable
-                quick_prompts = gr.Dataset(samples=sample_prompts, label='Sample Prompt', samples_per_page=1000, components=[prompt])
-                quick_prompts.click(lambda x: x[0], inputs=[quick_prompts], outputs=prompt, show_progress=False, queue=False)
-                submit_btn = gr.Button("Generate")
-                num_steps = gr.Slider(minimum=4, maximum=100, value=8, step=1, label="Step")
-                guidance = gr.Slider(minimum=0.0, maximum=10.0, value=1, step=0.1, label="Scale")
-            with gr.Column():
-                output_image = gr.Image(type="pil", label="Output Image")
-        submit_btn.click(fn=generate_image, inputs=[input_image,prompt,neg_prompt,guidance,num_steps], outputs=output_image)
-    block.launch()
-
-def launch_image_edit_low_app(model_path,dtype):
-    transformer = QwenImageTransformer2DModel.from_single_file(
-        model_path,
-        quantization_config=GGUFQuantizationConfig(compute_dtype=dtype),
-        torch_dtype=dtype,
-        config="callgg/image-edit-decoder",
-        subfolder="transformer"
-        )
-    text_encoder = Qwen2_5_VLForConditionalGeneration.from_pretrained(
-        "callgg/qi-decoder",
-        subfolder="text_encoder",
-        quantization_config=BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=dtype
-            ),
-        torch_dtype=dtype
-    )
-    text_encoder = text_encoder.to("cpu")
-    vae = AutoencoderKLQwenImage.from_pretrained(
-        "callgg/qi-decoder",
-        subfolder="vae",
-        torch_dtype=dtype
-        )
-    pipe = QwenImageEditPipeline.from_pretrained(
-            "callgg/image-edit-decoder",
-            transformer=transformer,
-            text_encoder=text_encoder,
-            vae=vae,
-            torch_dtype=dtype
-        )
-    pipe.enable_model_cpu_offload()
-    pipe.load_lora_weights("callgg/image-lite-lora", weight_name="lite.safetensors", adapter_name="lora")
-    def generate_image(image,prompt,neg_prompt,guidance_scale,num_steps):
-        if image is None or prompt.strip() == "":
-            return None
-        result = pipe(image=image,prompt=prompt,true_cfg_scale=guidance_scale,negative_prompt=neg_prompt,num_inference_steps=num_steps,
-            ).images[0]
-        return result
-    sample_prompts = ['add a hat to the subject',
-                    'convert to Ghibli style',
-                    'turn this image into line style']
-    sample_prompts = [[x] for x in sample_prompts]
-    block = gr.Blocks(title="gguf").queue()
-    with block:
-        gr.Markdown("## 🐷 Image Editor (low vram)")
         with gr.Row():
             with gr.Column():
                 input_image = gr.Image(type="pil", label="Input Image")
