febcae86271264872aa4feb609328492b524a2fe
calcuis <113646141+calcuis@users.noreply.github.com>
2025-09-23 04:14:11 -0800

    Update q4.py

--- src/gguf_connector/q4.py
+++ src/gguf_connector/q4.py
@@ -188,3 +188,60 @@ def launch_image_edit_app(model_path,dtype):
                 output_image = gr.Image(type="pil", label="Output Image")
         submit_btn.click(fn=generate_image, inputs=[input_image,prompt,neg_prompt,guidance,num_steps], outputs=output_image)
     block.launch()
+
+def launch_image_edit_plus_app(model_path,dtype):
+    transformer = QwenImageTransformer2DModel.from_single_file(
+        model_path,
+        quantization_config=GGUFQuantizationConfig(compute_dtype=dtype),
+        torch_dtype=dtype,
+        config="callgg/image-edit-plus",
+        subfolder="transformer"
+        )
+    text_encoder = Qwen2_5_VLForConditionalGeneration.from_pretrained(
+        "callgg/qi-decoder",
+        subfolder="text_encoder",
+        # torch_dtype=dtype
+        dtype=dtype
+    )
+    vae = AutoencoderKLQwenImage.from_pretrained(
+        "callgg/qi-decoder",
+        subfolder="vae",
+        torch_dtype=dtype
+        )
+    pipe = QwenImageEditPlusPipeline.from_pretrained(
+            "callgg/image-edit-plus",
+            transformer=transformer,
+            text_encoder=text_encoder,
+            vae=vae,
+            torch_dtype=dtype
+        )
+    pipe.enable_model_cpu_offload()
+    pipe.load_lora_weights("callgg/image-lite-lora", weight_name="lite.safetensors", adapter_name="lora")
+    def generate_image(image,prompt,neg_prompt,guidance_scale,num_steps):
+        if image is None or prompt.strip() == "":
+            return None
+        result = pipe(image=image,prompt=prompt,true_cfg_scale=guidance_scale,negative_prompt=neg_prompt,num_inference_steps=num_steps,
+            ).images[0]
+        return result
+    sample_prompts = ['add a hat to the subject',
+                    'convert to Ghibli style',
+                    'turn this image into line style']
+    sample_prompts = [[x] for x in sample_prompts]
+    # Gradio UI
+    block = gr.Blocks(title="gguf").queue()
+    with block:
+        gr.Markdown("## 🐷 Image Editor Plus")
+        with gr.Row():
+            with gr.Column():
+                input_image = gr.Image(type="pil", label="Input Image")
+                prompt = gr.Textbox(label="Prompt", placeholder="Enter your prompt here (or click Sample Prompt)", value="")
+                neg_prompt = gr.Textbox(label="Negative Prompt", value=" ", visible=False) # disable
+                quick_prompts = gr.Dataset(samples=sample_prompts, label='Sample Prompt', samples_per_page=1000, components=[prompt])
+                quick_prompts.click(lambda x: x[0], inputs=[quick_prompts], outputs=prompt, show_progress=False, queue=False)
+                submit_btn = gr.Button("Generate")
+                num_steps = gr.Slider(minimum=4, maximum=100, value=8, step=1, label="Step")
+                guidance = gr.Slider(minimum=0.0, maximum=10.0, value=1, step=0.1, label="Scale")
+            with gr.Column():
+                output_image = gr.Image(type="pil", label="Output Image")
+        submit_btn.click(fn=generate_image, inputs=[input_image,prompt,neg_prompt,guidance,num_steps], outputs=output_image)
+    block.launch()
