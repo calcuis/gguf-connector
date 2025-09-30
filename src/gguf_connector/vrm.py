c304f94309070fdad32630885b32f9a8fd44f28e
calcuis <113646141+calcuis@users.noreply.github.com>
2025-09-30 00:53:13 -0800

    Update vrm.py

--- src/gguf_connector/vrm.py
+++ src/gguf_connector/vrm.py
@@ -11,3 +11,11 @@ def get_gpu_vram(device: str | torch.device = "cuda", unit: str = "GiB") -> int:
         return memory // (1024**2)
     else:
         return memory
+
+def get_affordable_precision(device="cuda"):
+    if isinstance(device, str):
+        device = torch.device(device)
+    capability = torch.cuda.get_device_capability(0 if device.index is None else device.index)
+    sm = f"{capability[0]}{capability[1]}"
+    precision = "fp4" if sm == "120" else "int4"
+    return precision
