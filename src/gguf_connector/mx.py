0afa99e27216c6d8aa766da05cc4c9f798c2a657
calcuis <113646141+calcuis@users.noreply.github.com>
2026-05-02 01:26:12 -0700

    Update mx.py

--- src/gguf_connector/mx.py
+++ src/gguf_connector/mx.py
@@ -1,2 +1,2 @@
 import os
-os.system("vllm serve callgg/minimax-m2.7 \ --host 0.0.0.0 --port 8000 \ --served-model-name minimax-m2.7 \ --trust-remote-code \ --max-model-len 75776 \ --gpu-memory-utilization 0.85 \ --kv-cache-dtype fp8 \ --load-format fastsafetensors \ --enable-auto-tool-choice \ --tool-call-parser minimax_m2 \ --reasoning-parser minimax_m2_append_think")
+os.system("vllm serve callgg/minimax-m2.7 --host 0.0.0.0 --port 8000 --served-model-name minimax-m2.7 --trust-remote-code --max-model-len 75776 --gpu-memory-utilization 0.85 --kv-cache-dtype fp8 --load-format fastsafetensors --enable-auto-tool-choice --tool-call-parser minimax_m2 --reasoning-parser minimax_m2_append_think")
