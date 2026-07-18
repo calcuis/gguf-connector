203142c7f359fb3482edb2ae1cd9d2af971be98a
calcuis <113646141+calcuis@users.noreply.github.com>
2026-07-18 10:39:41 -0700

    Create et.py

---
+++ src/gguf_connector/et.py
@@ -0,0 +1,20 @@
+
+import os
+gguf_files = [file for file in os.listdir() if file.endswith('.gguf')]
+
+if gguf_files:
+    print('GGUF file(s) available. Select which one to edit:')
+    for index, file_name in enumerate(gguf_files, start=1):
+        print(f'{index}. {file_name}')
+    choice = input(f'Enter your choice (1 to {len(gguf_files)}): ')
+    try:
+        choice_index = int(choice) - 1
+        selected_file = gguf_files[choice_index]
+        print(f'Model file: {selected_file} is selected!')
+        input_path = selected_file
+        os.system(f'gguf-editor "{input_path}"')
+    except (ValueError, IndexError):
+        print('Invalid choice. Please enter a valid number.')
+else:
+    print('No GGUF files are available in the current directory.')
+    input('--- Press ENTER To Exit ---')
